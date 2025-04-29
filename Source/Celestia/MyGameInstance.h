025c01d3ef92b089243c0d0b83db17ce1e6725f4
Habin <backhabin1029@gmail.com>
2025-04-30 00:27:47 +0900

    커스텀 기능 마무리

--- Source/Celestia/MyGameInstance.h
+++ Source/Celestia/MyGameInstance.h
@@ -14,12 +14,5 @@ class CELESTIA_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 public:
-    // 플레이어 소지 금액
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    int32 Money;
-
-    // 인벤토리 맵 (예: 아이템 ID → 개수)
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    TMap<int32, int32> InventoryMap;
-	
+   
 };
